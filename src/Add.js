bcf59f4d91c7a46a63ecdfe942ffba3e2f3e747f
Anton Gunnarsson <gson.anton@hotmail.com>
2020-01-02 19:17:16 +0100

    Init

---
+++ src/Add.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Add() {
+  return (
+    <div>
+      <label>
+        Link
+        <input type="text"></input>
+      </label>
+      <label>
+        Note
+        <textarea></textarea>
+      </label>
+    </div>
+  )
+}
